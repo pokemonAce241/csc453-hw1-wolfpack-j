92ca68902d87ccf68874e359642efac4ff0b682b
Cameron Finley <cjfinley@ncsu.edu>
2019-02-20 14:59:44 -0500

    Add files via upload

--- Laptop2.py
+++ Laptop2.py
@@ -4,27 +4,38 @@ import datetime
 
 broker = "broker.hivemq.com"
 broker = "iot.eclipse.org"
+file = None
 
 MQTT_TOPIC = [("LightStatus",2),("Status/RaspberryPiA",2),("Status/RaspberryPiC",2),("threshold",2),("lightSensor",2)]
 MQTT_BROKER = "10.153.33.130"
 
 
 def on_connect(client, userdata, flags, rc):
+        fileName = raw_input("Filename: ")
+        if ( len(fileName) == 0 ):
+            fileName = "output.txt"
+        file = open(fileName, "w")
         print("connected with broker"+str(rc))
         client.subscribe(MQTT_TOPIC)
 
 def on_message(client, userdata, msg):
-	print(datetime.datetime.now(), end=' ', flush=True)
-	if msg.topic == "LightStatus":
+    print(datetime.datetime.now())
+    file.write(datetime.datetime.now())
+    if msg.topic == "LightStatus":
 		print("LightStatus: ",str(msg.payload.decode()))
-	if msg.topic == "Status/RaspberryPiA":
+		file.write("LightStatus: ",str(msg.payload.decode()))
+    if msg.topic == "Status/RaspberryPiA":
 		print("Status/RaspberryPiA: ",str(msg.payload.decode()))
-	if msg.topic == "Status/RaspberryPiC":
+		file.write("Status/RaspberryPiA: ",str(msg.payload.decode()))
+    if msg.topic == "Status/RaspberryPiC":
 		print("Status/RaspberryPiC: ",str(msg.payload.decode()))
-	if msg.topic == "threshold":
+		file.write("Status/RaspberryPiC: ",str(msg.payload.decode()))
+    if msg.topic == "threshold":
 		print("threshold: ",str(msg.payload.decode()))
-	if msg.topic == "lightSensor":
-		print("lightSensor: ",str(msg.payload.decode()))
+		file.write("threshold: ",str(msg.payload.decode()))
+    if msg.topic == "lightSensor":
+        print("lightSensor: ",str(msg.payload.decode()))
+        file.write("lightSensor: ",str(msg.payload.decode()))
 
 client = mqtt.Client("Laptop2")
 client.on_connect = on_connect
