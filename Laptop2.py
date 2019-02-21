93ad07042f176a4d9aeb573711d6ecbbcef08ea7
Stanton Parham <stparham@ncsu.edu>
2019-02-20 22:46:51 -0500

    Updated Laptop2 code and the associated README

--- Laptop2.py
+++ Laptop2.py
@@ -1,47 +1,88 @@
+# for MQTT
 import paho.mqtt.client as mqtt
 
+# for time stamps
 import datetime
 
-broker = "broker.hivemq.com"
-broker = "iot.eclipse.org"
+# for handling CTRL+C
+import signal, time
+
+# for getting command line arguments
+import sys, getopt
+
+# the topics to subscribe to
+TOPICS = [("LightStatus", 2), ("Status/RaspberryPiA", 2), ("Status/RaspberryPiC", 2), ("threshold", 2), ("lightSensor", 2)]
+
+# the name of the file where the logs will be written
+OUTPUT_FILE_NAME = "message_log.txt"
+
+# the file object used to write data
 file = None
 
-MQTT_TOPIC = [("LightStatus",2),("Status/RaspberryPiA",2),("Status/RaspberryPiC",2),("threshold",2),("lightSensor",2)]
-MQTT_BROKER = "10.153.33.130"
+# the IP address of the broker
+broker_ip = '127.0.0.1' # default to localhost
 
+# the MQTT client object
+client = None
+
+def main():
+    global broker_ip, client
+    # register the signal handler
+    signal.signal(signal.SIGINT, signal_handler)
+
+    # parse the command line arguments
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "i:")
+    except getopt.GetoptError:
+        print('GET OPT ERROR')
+    for opt, arg in opts:
+        if opt == '-i':
+            broker_ip = arg
+
+    client = mqtt.Client("Laptop2")
+    client.on_connect = on_connect
+    client.on_message = on_message
+
+    client.connect(broker_ip)
+    client.loop_start()
+    while True:
+        pass
 
 def on_connect(client, userdata, flags, rc):
-        fileName = raw_input("Filename: ")
-        if ( len(fileName) == 0 ):
-            fileName = "output.txt"
-        file = open(fileName, "w")
-        print("connected with broker"+str(rc))
-        client.subscribe(MQTT_TOPIC)
+        global file
+        print("Connected to broker at: " + broker_ip)
+        file = open(OUTPUT_FILE_NAME, "w")
+        client.subscribe(TOPICS)
 
 def on_message(client, userdata, msg):
-    print(datetime.datetime.now())
-    file.write(datetime.datetime.now())
+    global file
+    log = str(datetime.datetime.now())
     if msg.topic == "LightStatus":
-		print("LightStatus: ",str(msg.payload.decode()))
-		file.write("LightStatus: ",str(msg.payload.decode()))
+        log = log + " LightStatus: " + str(msg.payload.decode())
     if msg.topic == "Status/RaspberryPiA":
-		print("Status/RaspberryPiA: ",str(msg.payload.decode()))
-		file.write("Status/RaspberryPiA: ",str(msg.payload.decode()))
+        log = log + " Status/RaspberryPiA: " + str(msg.payload.decode())
     if msg.topic == "Status/RaspberryPiC":
-		print("Status/RaspberryPiC: ",str(msg.payload.decode()))
-		file.write("Status/RaspberryPiC: ",str(msg.payload.decode()))
+        log = log + " Status/RaspberryPiC: " + str(msg.payload.decode())
     if msg.topic == "threshold":
-		print("threshold: ",str(msg.payload.decode()))
-		file.write("threshold: ",str(msg.payload.decode()))
+        log = log + " threshold: " + str(msg.payload.decode())
     if msg.topic == "lightSensor":
-        print("lightSensor: ",str(msg.payload.decode()))
-        file.write("lightSensor: ",str(msg.payload.decode()))
+        log = log + " lightSensor: " + str(msg.payload.decode())
 
-client = mqtt.Client("Laptop2")
-client.on_connect = on_connect
-client.on_message = on_message
+    # print out the log and write it to the file
+    print(log)
+    file.write(log + "\n")
 
-client.connect(MQTT_BROKER)
-client.loop_start()
-while True:
-        pass
+# handle CTRL+C
+def signal_handler(sig, frame):
+    global client
+    print('\nDisconnecting gracefully')
+    if (client != None):
+        client.disconnect()
+    if (file != None):
+        file.close()
+    # wait before ending the program
+    time.sleep(0.5)
+    sys.exit(0)
+
+if __name__ == "__main__":
+    main()
--- Laptop2_README.md
+++ Laptop2_README.md
@@ -1,2 +1,11 @@
-Laptop2.py README
-Before runing Laptop2.py, change the defined MQTT_BROKER if the IP address has been changed. With the update of IP address, run Laptop2.py with console input "python Laptop2.py". Once Laptop2.py started, the logging process of broker will automatically start.
+# Laptop 2 README
+
+*All of the code was written using Python 3.  If the code is not running correctly, then make sure that you are using Python 3.  If Python 3 is not your default Python installation, then you will need to use the `pip3` command instead of `pip` and the `python3` command instead of `python`.*
+
+Laptop2.py requires the Python Paho MQTT client library.  Make sure that it is installed by following the steps below:
+1. Run `pip install paho-mqtt`.
+
+The Laptop2.py program accepts a command line argument for the broker's IP address.  It is specified by using the `-i` argument.  You can run the program by running the following command:
+`python Laptop2.py -i <IP_ADDRESS_HERE>`
+
+The program will print all logs to the console and print them out to a file called `message_log.txt`.  You can end the program gracefully by pressing CTRL + C.
